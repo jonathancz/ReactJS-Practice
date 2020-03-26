09712d4603d899bb09b7af7d6840d5b5c0f8b151
Ethan <ethan.chen@covenant.edu>
2020-03-25 23:09:55 -0400

    Functions as Props

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -11,13 +11,22 @@ class App extends Component {
       { name: 'Crystal', age: 25, belt: 'pink', id: 3},
     ]
   }
+
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja] // Spread operator to create a copy of the array
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
   render() {
     return(
       <div className="App">
         <h1>My first React App</h1>
         <p>Welcome :)</p>
         <Ninjas ninjas={this.state.ninjas}/>
-        <AddNinja />
+        <AddNinja addNinja={this.addNinja}/>
       </div>
     )
   }
